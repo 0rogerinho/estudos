c9c83a513861561d22eda22511bada95d8a5dcc9
0rogerinho <luaansilva11@gmail.com>
2023-02-26 23:57:25 -0500

    organização do conteudo estudado

---
+++ EstudoDeLinguagem/JS/Ex freecodeCamp/script.js
@@ -0,0 +1,8 @@
+// Configuração
+const myArray = [];
+
+// Altere apenas o código abaixo desta linha
+for (let c = 1; c < 10; c += 2) {
+  myArray.push(c);
+}
+console.log(myArray);
---
+++ Novos conhecimentos/js/FiltrarDeElemento(rocketseat)/script.js
@@ -0,0 +1,24 @@
+const filterElements = document.querySelector("#filter");
+
+const cards = document.querySelectorAll(".cards li");
+
+filterElements.addEventListener("input", filterCards);
+
+function filterCards() {
+  if (filterElements.value != "") {
+    for (let card of cards) {
+      let title = card.querySelector("h2");
+      title = title.textContent.toLowerCase();
+      let filterText = filterElements.value.toLowerCase();
+      if (!title.includes(filterText)) {
+        card.style.display = "none";
+      } else {
+        card.style.display = "block";
+      }
+    }
+  } else {
+    for (let card of cards) {
+      card.style.display = "block";
+    }
+  }
+}
