39b18fc8e105799ee229cee85d82f97cc58ea7fa
0rogerinho <luaansilva11@gmail.com>
2023-03-29 22:31:52 -0500

    estudo 103 freeCodeCap

--- EstudoDeLinguagem/JS/Ex freecodeCamp/script.js
+++ EstudoDeLinguagem/JS/Ex freecodeCamp/script.js
@@ -1,8 +1,56 @@
 // Configuração
-const myArray = [];
-
-// Altere apenas o código abaixo desta linha
-for (let c = 1; c < 10; c += 2) {
-  myArray.push(c);
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+/*
+function lookUpProfile(name, prop) {
+  // Altere apenas o código abaixo desta linha
+  let resultado = "";
+  contacts.forEach((element) => {
+    if (name === element.firstName && element.hasOwnProperty(prop)) {
+      resultado = {
+        name: element.firstName,
+      };
+      resultado[prop] = element[prop];
+      console.log(element);
+    }
+    // Altere apenas o código acima desta linha
+  });
+  console.log(resultado);
+  return resultado;
 }
-console.log(myArray);
+console.log(lookUpProfile("Harry", "likes"));
+*/
+function lookUpProfile(name, prop) {
+  resultado = contacts.find((element) => {
+    return element.firstName == name;
+  });
+  const teste = {};
+  teste.name = resultado.firstName;
+  teste[prop] = resultado[prop];
+  return teste;
+}
+console.log(lookUpProfile("Harry", "likes"));
